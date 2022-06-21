16a13cc5eb4cf12943206ea0c7d676a8de95acce
NoameBrabant <brabantnoame@gmail.com>
2022-06-21 15:27:39 +0200

    background + bird annimation

--- flappy_bird.py
+++ flappy_bird.py
@@ -4,7 +4,7 @@ import time
 import os
 import random
 
-WIN_WIDTH = 600
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
@@ -55,6 +55,8 @@ class Bird:
                 self.tilt -= self.ROT_VEL
     
     def draw(self, win):
+        self.img_count += 1 
+        
         if self.img_count < self.ANIMATION_TIME:
             self.img = self.IMGS[0]
         elif self.img_count < self.ANIMATION_TIME*2:
@@ -63,37 +65,41 @@ class Bird:
             self.img = self.IMGS[2]
         elif self.img_count < self.ANIMATION_TIME*4:
             self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME*4 + 1:
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
             self.img = self.IMGS[0]
             self.img_count = 0
         
         if self.tilt <= -80:
-            self.img = self.IMGS[1]
+            self.img = self.IMGS[1] 
             self.img_count = self.ANIMATION_TIME*2
 
-        rotated_image = pygame.trannsform.rotate(self.img, self.tilt)
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
         new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)     #StackOverflow
+        win.blit(rotated_image, new_rect.topleft)
     
     def getmask(self):
+
         return pygame.mask.from_surface(self.img)
 
 def draw_window (win, bird):
-    win.blit(BG_IMG,(0,0))
-    bird.draw()
+    win.blit(BG_IMG, (0, 0))
+    bird.draw(win)
     pygame.display.update()
 
 def main():
     bird = Bird(200,200)
-    win = pygame.display.set_mode(WIN_WIDTH, WIN_HEIGHT)
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window(win,bird)
+        draw_window(win, bird)
     pygame.quit()
     quit()
-        
+
+main() 
 
 
 
